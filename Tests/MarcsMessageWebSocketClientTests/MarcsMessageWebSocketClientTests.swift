6ee3bad45a884405556b083aec88be765ecd69a9
Marc Stroebel <mdstroebel@gmail.com>
2022-07-03 22:11:21 +1000

    Initial Commit

---
+++ Tests/MarcsMessageWebSocketClientTests/MarcsMessageWebSocketClientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MarcsMessageWebSocketClient
+
+final class MarcsMessageWebSocketClientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MarcsMessageWebSocketClient().text, "Hello, World!")
+    }
+}
